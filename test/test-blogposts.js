285a1112ea4f1d3f64c8e7bab36cc068d06da1d7
Jen Von Essen <cgajen09@gmail.com>
2017-03-31 16:02:41 -0700

    half way there, ish

---
+++ test/test-blogposts.js
@@ -0,0 +1,84 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('BlogPosts', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/blogposts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+        const expectedKeys = ["id", "title", "content", "author", "publishDate"];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+  it('should add an item on POST', function() {
+    const newItem = {title: "New Post", content:"This is new content", author:"Bob"};
+    return chai.request(app)
+      .post('/blogposts')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys("id", "title", "content", "author", "publishDate");
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  it('should update items on PUT', function() {
+    const updateData = {
+      title: "Boring Update",
+      content: "Boring updated content.",
+      author: "JimBob"
+    };
+
+    return chai.request(app)
+      .get('/blogposts')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/blogposts/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateData);
+      });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/blogposts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blogposts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
